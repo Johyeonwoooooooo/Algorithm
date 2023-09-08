6db7be11cdcf03f099b375071a455fdcb789c3e8
HYEONWOO JO <130330546+Johyeonwoooooooo@users.noreply.github.com>
2023-09-08 16:18:33 +0900

    Add files via upload

---
+++ 09/9-6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n != 1)
+	{
+		for (int i = 2; i <= n; i++)
+		{
+			while (true)
+			{
+				if (n % i != 0)
+					break;
+				cout << i << endl;
+				n /= i;
+			}
+		}
+	}
+	return 0;
+}
